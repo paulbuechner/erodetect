03c8e93672facccd8da3344a5e2fed5247d75014
Paul Büchner <paul-buechner@outlook.de>
2021-10-21 22:27:46 +0200

    fix(yolor): update `bccd` download

--- src/models/yolor/scripts/get_bccd.sh
+++ src/models/yolor/scripts/get_bccd.sh
@@ -9,7 +9,7 @@
 
 # Download/unzip labels
 d='../data/bccd/' # unzip directory
-url=https://public.roboflow.com/ds/OD25lP5osO?key=XlW7qHAm5F
+url=https://public.roboflow.com/ds/0e3by4kfHe?key=6IcrKmhw11
 f='bccd.zip' # MB
 echo 'Downloading' $url$f ' ...'
 curl -L $url -o $f && unzip -q $f -d $d && rm $f & # download, unzip, remove in background
