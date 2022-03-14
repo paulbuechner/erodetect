0751c60e0234e78e11cf858bccb1f2a1b07b9c08
Paul Büchner <paul-buechner@outlook.de>
2022-03-14 22:04:46 +0100

    feat(model): updatetd `unzip` util

--- models/utils/unzip.py
+++ models/utils/unzip.py
@@ -1,6 +1,18 @@
 import os
 import argparse
 import zipfile
+import shutil
+
+def copy_files(src, dest):
+    src_files = os.listdir(src)
+
+    for file_name in src_files:
+        full_file_name = os.path.join(src, file_name)
+        if os.path.isfile(full_file_name):
+            if not os.path.exists(dest):
+                os.makedirs(dest)
+
+            shutil.copy(full_file_name, dest)
 
 # Unzip all files in given dir into specifed dir and process it's content
 if __name__ == "__main__":
@@ -8,7 +20,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--from",
+        "--path",
         type=str,
         default=cwd,
         help="dir path containing the zip files (default: current working dir)",
@@ -28,10 +40,26 @@ if __name__ == "__main__":
     path = args.path
     files = os.listdir(path)
 
+    # Unzip all files in given dir
     for file in files:
         if file.endswith(".zip"):
-            filePath = path + "/" + file
+            filePath = f"{path}/{file}"
             zip_file = zipfile.ZipFile(filePath)
+
             for names in zip_file.namelist():
-                zip_file.extract(names, path)
+                zip_file.extract(names, f"{args.to}/Temp")
             zip_file.close()
+
+            # Copy train, valid and test images to train, valid and test dir
+            copy_files(f"{args.to}/Temp/train/images", f"{args.to}/train/images")
+            copy_files(f"{args.to}/Temp/train/labels", f"{args.to}/train/labels")
+            copy_files(f"{args.to}/Temp/test/images", f"{args.to}/test/images")
+            copy_files(f"{args.to}/Temp/test/labels", f"{args.to}/test/labels")
+            copy_files(f"{args.to}/Temp/valid/images", f"{args.to}/valid/images")
+            copy_files(f"{args.to}/Temp/valid/labels", f"{args.to}/valid/labels")
+
+            # Copy data.yaml
+            shutil.copy(f"{args.to}/Temp/data.yaml", f"{args.to}/data.yaml")
+
+            print(f"{file} unzipped")
+
